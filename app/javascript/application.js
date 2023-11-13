3fd6b2f52aefe403d82a78231608395b40b826d4
ga <ga@gas-MacBook-Pro.local>
2023-11-13 10:52:37 -0500

    jquery and bootstrap conf

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "jquery"
+import "jquery_ujs"
+import "popper"
+import "bootstrap"
 import "@hotwired/turbo-rails"
 import "controllers"
