985fbe44c9d02822b99d6948a980165461c1b631
ga <ga@gas-MacBook-Pro.local>
2023-11-13 10:59:11 -0500

    adding weather controller, views and specs.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'weather/index'
+  post 'weather/retrieve'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+   root to: "weather#index"
 end
