e2056faaac1a82eee8505208409fe1865768caa4
ga <ga@gas-MacBook-Pro.local>
2023-11-21 17:51:02 -0500

    retrieve in turbo rails

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -4,5 +4,9 @@ class WeatherController < ApplicationController
   # from remote form.
   def retrieve
     @forecast = Forecast.get_for(params[:address])
+
+    respond_to do |format|
+      format.turbo_stream
+    end
   end
 end
